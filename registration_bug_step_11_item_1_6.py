f021087921be327d2e83b5549372b65012fd2dc3
mr_dyman <mr_dyman@mail.ru>
2021-10-20 16:42:45 +0300

    added script for checking registration form

---
+++ registration_bug_step_11_item_1_6.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+import time
+
+try:
+    link = "http://suninjuly.github.io/registration2.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    # Ваш код, который заполняет обязательные поля
+    first_name = browser.find_element_by_class_name('first_block .form-group:nth-child(1) input.first')
+    first_name.send_keys('Ivan')
+
+    second_name = browser.find_element_by_class_name('first_block .form-group:nth-child(2) input.second')
+    second_name.send_keys('Petrov')
+
+    third_name = browser.find_element_by_class_name('first_block .form-group:nth-child(3) input.third')
+    third_name.send_keys('test@email.com')
+
+    # Отправляем заполненную форму
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+
+    # Проверяем, что смогли зарегистрироваться
+    # ждем загрузки страницы
+    time.sleep(1)
+
+    # находим элемент, содержащий текст
+    welcome_text_elt = browser.find_element_by_tag_name("h1")
+    # записываем в переменную welcome_text текст из элемента welcome_text_elt
+    welcome_text = welcome_text_elt.text
+
+    # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+    assert "Congratulations! You have successfully registered!" == welcome_text
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
